86dc51772983c75e27f5e979941f229aece596cc
GO-xiaoqin <59362515+GO-xiaoqin@users.noreply.github.com>
2021-01-16 14:38:24 +0800

    Update conf.py
    v 1.74

--- conf.py
+++ conf.py
@@ -2,13 +2,13 @@
 # date: 21-1-12 上午9:44
 # 统一配置文件
 # redis
-REDIS_URL = "redis://:Xu551212@127.0.0.1/0"
+REDIS_URL = "redis://user:pwd@127.0.0.1/0"
 
 # mysql
 MYSQL_HOST = '127.0.0.1'
 MYSQL_PORT = 3306
 MYSQL_USER = 'root'
-MYSQL_PWD = 'Xu551212'
+MYSQL_PWD = pwd
 MYSQL_DB = 'test'
 
 # app.py 启动配置
